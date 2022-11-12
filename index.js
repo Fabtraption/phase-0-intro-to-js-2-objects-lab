1554dcf8b6f7e2b4a28b42efdb74e11c1059e045
Chris Bonet <christopher.bonet@gmail.com>
2022-11-11 19:11:52 -0500

    commit tree ddc5bafc60288507dfecb9b3a8dc926d758779fd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name : "Chris Bonet",
+    streetAddress : "74-12 35th Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployees = { ...object};
+    newEmployees[key] = value;
+    return newEmployees;
+}; //DONE//
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+}; //DONE//
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployees = { ...object };
+    delete newEmployees[key];
+    return newEmployees;
+};
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+};
